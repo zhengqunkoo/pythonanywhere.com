dc17536b8a9522636514d34c0c74bf0398e61daf
zhengqunkoo <zhengqun.koo@gmail.com>
2025-11-25 06:53:04 +0000

    Add password protection in-app instead of through PythonAnywhere (they probably provide password protection through nginx)

--- flask_app.py
+++ flask_app.py
@@ -2,18 +2,35 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+auth = HTTPBasicAuth()
+USER_CREDENTIALS = {}
+USER_CREDENTIALS['username'] = 'zhengqunkoo'
+# Store hashed password (in a real application, save this securely in a database)
+USER_CREDENTIALS['hashed_password'] = 'scrypt:32768:8:1$pLZfdLYlJ9sI8hZr$0894222e1765b0d212e11941441624f5fe9c606d8f0683187e9cab9373f23b80e25ee71a0c160305ae4dacff2abd3afd1562c2a692491d69d16edd9b5e095fa2' #generate_password_hash("password123")
+
 comments = []
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return redirect(url_for('handle_comments'))
+
+@auth.verify_password
+def login(username, password):
+    return username == USER_CREDENTIALS['username'] and check_password_hash(USER_CREDENTIALS['hashed_password'], password)
+
+@app.route('/comment_page', methods=['GET', 'POST'])
+@auth.login_required
+def handle_comments():
     if request.method == 'GET':
         return render_template('main_page.html', comments=comments)
     elif request.method == 'POST':
         comments.append(request.form['comment'])
         return redirect(url_for('index'))
     else:
-        raise Exception('Unknown request method')
+        return 'Unknown request method'
