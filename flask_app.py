ed9fe59b3782ea87603713ea33f24c6d7216ead6
zhengqunkoo <zhengqun.koo@gmail.com>
2025-11-26 09:22:58 +0000

    Add database schema

--- flask_app.py
+++ flask_app.py
@@ -27,6 +27,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
 comments = []
 
 @app.route('/')
