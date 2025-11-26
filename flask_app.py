26c7971ec9ab0ea5231873485a0967c6d51e40ac
zhengqunkoo <zhengqun.koo@gmail.com>
2025-11-26 08:52:17 +0000

    Add database connector

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -13,6 +14,18 @@ USER_CREDENTIALS = {}
 USER_CREDENTIALS['username'] = 'zhengqunkoo'
 # Store hashed password (in a real application, save this securely in a database)
 USER_CREDENTIALS['hashed_password'] = 'scrypt:32768:8:1$pLZfdLYlJ9sI8hZr$0894222e1765b0d212e11941441624f5fe9c606d8f0683187e9cab9373f23b80e25ee71a0c160305ae4dacff2abd3afd1562c2a692491d69d16edd9b5e095fa2' #generate_password_hash("password123")
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}'.format(
+    username='zhengqunkoo',
+    password='R3d!f7VxPq2Z',
+    hostname='zhengqunkoo.mysql.pythonanywhere-services.com',
+    databasename='zhengqunkoo$comments'
+)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
